29eaf93244851a563d8bb836a56be84de905d346
Uku91 <166671311+Uku91@users.noreply.github.com>
2025-07-16 07:15:20 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+/**
+ * MINI PROJECT ASSESSMENT OBJECTIVE:
+ * ----------------------------------
+ * The goal of this assessment is to set up a basic React project 
+ * and render a simple "Hello World" message in the web browser.
+ * This demonstrates the ability to:
+ * 1. Initialize a React project using Vite/CRA
+ * 2. Understand JSX syntax and functional components
+ * 3. Render content semantically with <h1>
+ * 
+ * IMPLEMENTATION:
+ * - Used React 18+ functional component
+ * - Deployed with Vite for zero-config setup
+ * - StrictMode enabled for best practices
+ **/ 
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+   <div>
+   <h1>Hello World</h1>
+   </div>
+    </>
+  );
+}
+
+export default App
